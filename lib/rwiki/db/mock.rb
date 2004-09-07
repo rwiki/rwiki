c5f6036f06876ae43c85f5cbb31d5bf520b20611
Masatoshi SEKI <m_seki@mac.com>
2004-09-07 15:19:40 +0000

    add db/mock.rb
    git-svn-id: http://www.cozmixng.org/repos/rwiki/trunk@2 6ac77252-39e2-0310-ae13-de3c1d0591de

---
+++ lib/rwiki/db/mock.rb
@@ -0,0 +1,83 @@
+# -*- indent-tabs-mode: nil -*-
+
+require 'monitor'
+require 'rwiki/db/base'
+
+module RWiki
+  module DB
+    class Mock < Base
+      include MonitorMixin
+
+      class Entry
+        def initialize(old, src)
+          @revision = old ? old.revision : 1
+          set_src(src)
+        end
+        attr_reader :revision, :src, :modified
+
+        private
+        def set_src(src)
+          @src = src
+          @modified = Time.now
+          @revision += 1
+        end
+      end
+
+      class NullEntry
+        def revision; nil; end
+        def src; nil; end
+        def modified; nil; end
+      end
+
+      def initialize(other = nil)
+        super()
+        @db = Hash.new
+        @null_entry = NullEntry.new
+      end
+
+      private
+      def set(k, v, opt=nil)
+        return if v.nil?
+        synchronize do
+          if v.empty?
+            @db.delete(k)
+          else
+            @db[k] = Entry.new(@db[k], v)
+          end
+        end
+        nil
+      end
+
+      def get(k)
+        fetch_entry(k) do |entry|
+          return entry.src
+        end
+      end
+
+      def fetch_entry(k)
+        synchronize do
+          return yield(@db.fetch(k, @null_entry))
+        end
+      end
+      
+      public
+      def modified(k)
+        fetch_entry(k) do |entry|
+          return entry.modified
+        end
+      end
+
+      def revision(k)
+        fetch_entry(k) do |entry|
+          return entry.revision
+        end
+      end
+
+      def each
+        @db.keys.each do |name|
+          yield(name)
+        end
+      end
+    end
+  end
+end
---
+++ test/test_db_mock.rb
@@ -0,0 +1,24 @@
+require 'test/unit'
+require 'rwiki/db/mock'
+
+class TestDBMock < Test::Unit::TestCase
+  def setup
+  end
+
+  def teardown
+  end
+
+  def test_db_mock
+    db = RWiki::DB::Mock.new
+    db['top'] = "= Top\n"
+    assert_equal("= Top\n", db['top'])
+    rev = db.revision('top')
+    db['top', rev] = "= Top2\n"
+    assert_equal("= Top2\n", db['top'])
+    assert(rev != db.revision('top'))
+
+    assert_equal(nil, db['test'])
+    assert_equal(nil, db.revision('test'))
+    assert_equal(nil, db.modified('test'))
+  end
+end
