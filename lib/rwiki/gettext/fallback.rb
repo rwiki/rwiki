bc730f194560681a89700be10b03ffffa91e323a
Kouhei Sutou <kou@cozmixng.org>
2004-10-04 14:19:33 +0000

    * added fallback code for missing gettext.
    git-svn-id: http://www.cozmixng.org/repos/rwiki/trunk@14 6ac77252-39e2-0310-ae13-de3c1d0591de

---
+++ lib/rwiki/gettext/fallback.rb
@@ -0,0 +1,22 @@
+# -*- indent-tabs-mode: nil -*-
+
+module RWiki
+  module GetText
+
+    class << self
+      def set_locale(locale=nil)
+        # do nothing
+      end
+
+      def set_charset(charset)
+        # do nothing
+      end
+
+      def gettext(msgid)
+        msgid
+      end
+      
+    end
+    
+  end
+end
