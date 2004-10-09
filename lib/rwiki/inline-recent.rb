012968701eebc0a16962b1689e20275d0743145f
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2004-10-09 18:19:25 +0000

    ERbLoader -> ERBLoader
    git-svn-id: http://www.cozmixng.org/repos/rwiki/trunk@18 6ac77252-39e2-0310-ae13-de3c1d0591de

--- lib/rwiki/inline-recent.rb
+++ lib/rwiki/inline-recent.rb
@@ -2,6 +2,6 @@ RWiki::Version.regist('rwiki/inline-recent', '2004-02-09')
 
 module RWiki
   class PageFormat
-    @rhtml[:inline_recent] = ERbLoader.new('inline_recent(pg)', 'inline-recent.rhtml')
+    @rhtml[:inline_recent] = ERBLoader.new('inline_recent(pg)', 'inline-recent.rhtml')
   end
 end
