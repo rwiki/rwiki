3f4f4b869b6c31729e1984768e6473c59764d09d
Masatoshi SEKI <m_seki@mac.com>
2004-11-22 08:42:24 +0000

    import StoryCard
    git-svn-id: http://www.cozmixng.org/repos/rwiki/trunk@27 6ac77252-39e2-0310-ae13-de3c1d0591de

---
+++ lib/rwiki/story-inline-test.rb
@@ -0,0 +1,58 @@
+require 'erb'
+require 'rwiki/rd/ext/base'
+
+module RD
+  module Ext
+    class InlineVerbatim
+    def test_result_form(name)
+      <<EOS
+<%
+  now = Time.now
+  today = Time.local(now.year, now.month, now.day)
+  it = pg.book['#{name}'].prop(:story)
+
+  if it
+    begin
+      v = it[:test_result][today]
+      ok_selected = v ? "selected" : ""
+      ng_selected = v == false ? "selected" : ""
+    rescue
+      ok_selected = ""
+      ng_selected = ""
+    end
+%> 
+      <form action="<%= form_action() %>#<%=u(it[:name])%>" method="post">
+        <input type="hidden" name="cmd" value="view" />
+        <input type="hidden" name="name" value="<%=u(it[:name])%>" />
+        <input type="hidden" name="story" value="test" />
+        <select name="testresult" onchange="this.form.submit()">
+         <option value="">-</option>
+         <option value="OK" <%=ok_selected%>>OK</option>
+         <option value="NG" <%=ng_selected%>>NG</option>
+        </select>
+        <input type="submit" value="Set" />
+       </form>
+<%
+  end
+%>
+EOS
+    end
+
+    def ext_inline_verb_test(label, content, visitor)
+      label = label.to_s
+      return nil unless /^test:\s*(.+)$/ =~ label
+      name = $1
+      # @links.push name.to_a unless @links.include?(name.to_a)
+      visitor.links.push name.to_a unless visitor.links.include?(name.to_a)
+      ref = %Q[<%= ref_name('#{CGI.escapeHTML(name)}')%>]
+      anchor = %Q[<a href="#{ref}">#{CGI.escapeHTML(name)}</a>]
+      %Q[<h3>#{anchor}</h3><%= pg.book['#{name}'].prop(:story)[:test_inline] rescue '' %>] + test_result_form(name)
+    end
+    def self.about_ext_inline_verb_test
+      h(%Q!inline image (example: (('test:name')))!)
+    end
+  end
+end
+end
+
+# RD::RD2RWikiVisitor.install_extension
