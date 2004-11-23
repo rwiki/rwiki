f1106165f176b6f2bacc6c224ade355bdcf05e9b
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2004-11-23 09:36:29 +0000

    test/test_format.rb: test_modified and test_ref_name added.
    git-svn-id: http://www.cozmixng.org/repos/rwiki/trunk@31 6ac77252-39e2-0310-ae13-de3c1d0591de

--- test/test_format.rb
+++ test/test_format.rb
@@ -28,5 +28,37 @@ class TestFormat < Test::Unit::TestCase
     assert_equal(expected, actual)
   end
 
+  def assert_modified(expected, diff)
+    assert_equal(expected, @format.modified(Time.now - diff))
+  end
+
+  def test_modified
+    assert_equal('-', @format.modified(nil))
+    assert_equal("0m", @format.modified(Time.now))
+    assert_modified("1m", 61)
+    assert_modified("1m", 90)
+    assert_modified("1m", 119)
+    assert_modified("2m", 121)
+    assert_modified("60m", 60*60)
+    assert_modified("1h", 61*60)
+    assert_modified("24h", 24*60*60)
+    assert_modified("24h", 25*60*60-1)
+    assert_modified("1d", 25*60*60+1)
+    assert_modified("367d", 367*24*60*60)
+  end
+
+  def test_ref_name
+    expected = ref_name("test name")
+    actual = @format.ref_name("test name")
+    assert_equal(expected, actual)
+
+    expected = ref_name('test name <&">', 'key <&">;'=>'value <&">;')
+    actual = @format.ref_name('test name <&">', 'key <&">;'=>'value <&">;')
+    assert_equal(expected, actual)
+
+    expected = ref_name('test name <&">', {}, 'edit')
+    actual = @format.ref_name('test name <&">', {}, 'edit')
+    assert_equal(expected, actual)
+  end
 end
 
