d552ed6869d8003637fd435aa89fe25c9c614934
Kouhei Sutou <kou@cozmixng.org>
2005-03-13 15:36:52 +0000

    * supported RSS output by RSS Maker.
    git-svn-id: http://www.cozmixng.org/repos/rwiki/trunk@161 6ac77252-39e2-0310-ae13-de3c1d0591de

--- lib/rwiki/rss/writer.rb
+++ lib/rwiki/rss/writer.rb
@@ -33,32 +33,109 @@ module RWiki
         %Q|<span class="navi">[<a href="#{ ref_name(pg.name, {'navi' => pg.name}, 'rss') }">#{ h title }</a>]</span>|
       end
 
-      private
-      @rhtml = {
-        :rss => ERBLoader.new('rss(pg)', ['rss', 'recent1.0.rrdf'])
-      }
-      reload_rhtml
+      begin
+        require "rss/maker"
+        
+        def rss(pg)
+          rec_chan = recent_changes(pg)
+          full_rss_url = full_ref_name(::RWiki::RSS::PAGE_NAME, {}, "rss")
+          full_top_url = full_ref_name(::RWiki::TOP_NAME)
 
+          rss = ::RSS::Maker.make("1.0") do |maker|
+            maker.encoding = @@charset
+            
+            maker.channel.about = full_rss_url
+            maker.channel.title = @@title
+            maker.channel.link = full_top_url
+            maker.channel.description = @@description
+            maker.channel.dc_language = @@lang
+            maker.channel.dc_date = pg.book[::RWiki::TOP_NAME].modified
+            maker.channel.dc_publisher = @@address if @@address
+            maker.channel.dc_creator = @@mailto if @@mailto
+            
+            if image
+              maker.image.title = @@title
+              maker.image.url = image
+            end
+
+            rec_chan.each do |page|
+              item = maker.items.new_item
+              item.title = page.title
+              item.link = full_ref_name(page.name)
+              item.description = description_value(page)
+              item.dc_date = page.modified
+              item.content_encoded = content_encoded_value(page)
+            end
+
+            maker.textinput.title = @@title
+            maker.textinput.description = "Search #{@@description}"
+            maker.textinput.name = "key"
+            maker.textinput.link = full_ref_name("search")
+          end
+
+          rss.to_s
+        end
+      rescue LoadError
+        @rhtml = {
+          :rss => ERBLoader.new('rss(pg)', ['rss', 'recent1.0.rrdf'])
+        }
+        reload_rhtml
+      end
+
+      private
+      def recent_changes(pg)
+        key = "pages"
+        default = 30
+        num, range, have_more = limit_number(key, default, pg.book.size)
+        rec_chan = pg.book.recent_changes[range]
+        rec_chan.reject!{|page| not page.modified.kind_of?(Time)}
+        rec_chan
+      end
+      
       def content_encoded(page)
+        value = content_encoded_value(page)
+        if value
+          %Q|<content:encoded>#{h value}</content:encoded>|
+        end
+      end
+
+      def content_encoded_value(page)
         diff = page.latest_diff
         if diff and /\A\s*\z/ !~ diff
           page.latest_formatted_diff ||= format_diff(page.name, diff)
-          content = page.latest_formatted_diff
-          %Q|<content:encoded>#{h content}</content:encoded>|
+          page.latest_formatted_diff
         else
-          ''
+          nil
         end
       end
       
       def description(page)
-        if /\A\s*\z/ !~ page.log.to_s
-          %Q|<description>#{h page.log}</description>|
+        value = description_value(page)
+        if value
+          %Q|<description>#{h value}</description>|
         end
       end
 
+      def description_value(page)
+        if /\A\s*\z/ !~ page.log.to_s
+          page.log
+        else
+          nil
+        end
+      end
+      
       def dc_date(page)
+        value = dc_date_value(page)
+        if value
+          %Q|<dc:date>#{h value}</dc:date>|
+        end
+      end
+
+      def dc_date_value(page)
         if page.modified
-          %Q|<dc:date>#{h page.modified.gmtime.iso8601}</dc:date>|
+          page.modified.gmtime.iso8601
+        else
+          nil
         end
       end
     end
