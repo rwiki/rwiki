c4a3149259509375e530ea88887eac3226aca41a
Kouhei Sutou <kou@cozmixng.org>
2005-03-31 00:47:36 +0000

    * po/update_mo.rb: changed output directory      from "locale/#{lang}/LC_MESSAGES"   to "data/locale/#{lang}/LC_MESSAGES".
    * install.rb:
      - supported installing under data/ directory.
      - new option: --datadir.
      - used fileutils instead of ftools.
    
    
    git-svn-id: http://www.cozmixng.org/repos/rwiki/trunk@185 6ac77252-39e2-0310-ae13-de3c1d0591de

--- po/update_mo.rb
+++ po/update_mo.rb
@@ -8,7 +8,7 @@ require "po/common"
 
 LANGS.each do |lang|
   po = File.join(PO_DIR, "#{lang}.po")
-  mo_dir = File.join("locale", lang, "LC_MESSAGES")
+  mo_dir = File.join("data", "locale", lang, "LC_MESSAGES")
   mo = File.join(mo_dir, "#{BASE_NAME}.mo")
   FileUtils.mkdir_p(mo_dir)
   run("msgfmt", "-o", mo, po)
