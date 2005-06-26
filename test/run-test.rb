e3e0dd332d743772908cb6bd717cfca803b37c7c
Kouhei Sutou <kou@cozmixng.org>
2005-06-26 03:24:08 +0000

    * test/run-test.rb: fixed negative condition.
    git-svn-id: http://www.cozmixng.org/repos/rwiki/trunk@225 6ac77252-39e2-0310-ae13-de3c1d0591de

--- test/run-test.rb
+++ test/run-test.rb
@@ -29,7 +29,7 @@ Dir.glob(tests) do |test|
 end
 
 if Test::Unit::AutoRunner.respond_to?(:standalone?)
-  exit Test::Unit::AutoRunner.run($0, File.dirname($0))
-else
   exit Test::Unit::AutoRunner.run(false, File.dirname($0))
+else
+  exit Test::Unit::AutoRunner.run($0, File.dirname($0))
 end
