0b388121489f00bfc4e600879f2b819bce44a814
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2005-06-29 19:34:19 +0000

    add update script
    git-svn-id: http://www.cozmixng.org/repos/rwiki/trunk@232 6ac77252-39e2-0310-ae13-de3c1d0591de

---
+++ site/ruby-lang/update.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -ex
+test -d /var/lib/ruby-man
+test -w /var/lib/ruby-man
+test -x /var/lib/ruby-man
+test -O /var/lib/ruby-man
+cd $(dirname $0)
+rm -vrf /var/lib/ruby-man/site
+mkdir /var/lib/ruby-man/site
+cp -v site/*.* /var/lib/ruby-man/site
+rm -vrf /var/lib/ruby-man/web
+mkdir /var/lib/ruby-man/web
+cp -v web/*.* /var/lib/ruby-man/web
+cd $(dirname $0)/../..
+rm -rf /var/lib/ruby-man/lib/rt
+rm -rf /var/lib/ruby-man/lib/rwiki
+ruby -d install.rb -d /var/lib/ruby-man/lib --datadir /var/lib/ruby-man/data
