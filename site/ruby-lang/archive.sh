0cb919070f67da8b285bf1da51f5259fd4098b98
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2005-08-29 15:59:04 +0000

    ruby-man archive publish script
    git-svn-id: http://www.cozmixng.org/repos/rwiki/trunk@259 6ac77252-39e2-0310-ae13-de3c1d0591de

---
+++ site/ruby-lang/archive.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+set -ex
+
+# config
+WORKDIR="$(dirname $0)"
+ARCHIVEDIR=/var/www/www.ruby-lang.org/ja/man/archive/
+FTPDOCDIR=/home/ftp/pub/ruby/doc/
+
+HTMLDIR=ruby-man-ja-html-$(date +'%Y%m%d')
+RDDIR=ruby-man-ja-rd-$(date +'%Y%m%d')
+
+RWIKIURI='druby://localhost:7429'
+TOPPAGENAME=$(ruby -e 'print "Ruby\245\352\245\325\245\241\245\354\245\363\245\271\245\336\245\313\245\345\245\242\245\353"')
+# needs trailing slash (for rsync)
+RDSRCDIR=/var/lib/ruby-man/man-rd-ja/
+
+TAR_COMMAND='tar --owner=root --group=root'
+P7ZIP_COMMAND=/home/kazu/p7zip_4.20/bin/7za
+
+
+# override
+HTMLDIR=ruby-man-ja-html-20050829
+RDDIR=ruby-man-ja-rd-20050829
+
+
+fetching () {
+  cd "$WORKDIR"
+
+  rsync -avzC "$RDSRCDIR" "$RDDIR"
+  mkdir "$HTMLDIR"
+  ../../interface/fetch_static_html.rb "$RWIKIURI" "$TOPPAGENAME" "$HTMLDIR"
+}
+
+packing () {
+  cd "$WORKDIR"
+
+
+  for CONTENTDIR in "$HTMLDIR" "$RDDIR"; do
+    $TAR_COMMAND -jcvf "$CONTENTDIR".tar.bz2 "$CONTENTDIR"
+    $TAR_COMMAND -zcvf "$CONTENTDIR".tar.gz "$CONTENTDIR"
+    $P7ZIP_COMMAND a "$CONTENTDIR".zip "$CONTENTDIR"
+  done
+}
+
+publishing_to_http () {
+  cp -v "$HTMLDIR".* "$RDDIR".* "$ARCHIVEDIR"
+  cd "$ARCHIVEDIR"
+  md5sum ruby* > MD5SUM.txt
+}
+
+publishing_to_ftp () {
+  cp -v "$HTMLDIR".* "$RDDIR".* "$FTPDOCDIR"
+  cd "$FTPDOCDIR"
+  md5sum ruby* > MD5SUM.txt
+}
+
+default_main () {
+  fetching
+  packing
+  publishing_to_http
+  publishing_to_ftp
+}
+
+default_main
