0153cd5494121f5f2b0ee80e3bc02367dfcaece1
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2005-10-29 08:52:53 +0000

    add missing file
    git-svn-id: http://www.cozmixng.org/repos/rwiki/trunk@283 6ac77252-39e2-0310-ae13-de3c1d0591de

---
+++ lib/rwiki/static_view_filename.rb
@@ -0,0 +1,26 @@
+# -*- indent-tabs-mode: nil -*-
+
+module RWiki
+  module URLGenerator
+    module StaticView
+
+      def underline_html(name)
+        name = name.gsub(/([^a-zA-Z0-9.\-]+)/n) do
+          '_' + $1.unpack('H2' * $1.size).join('_').upcase
+        end
+        sprintf("%s.html", name)
+      end
+
+      def ja_man_html(name)
+        return "methodlist" if name == "method"
+        name = name.dup
+        name.tr_s!(' -/:-@\[-`{-~', '_')
+        name.gsub!(/([^a-zA-Z0-9_]+)/n) do
+          '_' + $1.unpack('H2' * $1.size).join('').upcase
+        end
+        sprintf("%s.html", name.squeeze('_'))
+      end
+
+   end
+  end
+end
