a14da1ad7774fc6beb3aad69d441476c73d4c4fc
Masatoshi SEKI <m_seki@mac.com>
2005-11-06 13:34:31 +0000

    add content cache
    git-svn-id: http://www.cozmixng.org/repos/rwiki/trunk@290 6ac77252-39e2-0310-ae13-de3c1d0591de

---
+++ lib/rwiki/content-cache.rb
@@ -0,0 +1,82 @@
+# -*- indent-tabs-mode: nil -*-
+
+require 'rwiki/page'
+require 'digest/md5'
+
+module RWiki
+  class ContentCache
+    def initialize(dir="cache")
+      @dir = dir
+      Dir.mkdir(@dir)
+    rescue Errno::EEXIST
+    end
+
+    def fetch(name, filename, sign)
+      File.open(filename, 'rb') do |f|
+        return nil unless name == Marshal.load(f)
+        return nil unless sign == Marshal.load(f)
+        return Marshal.load(f)
+      end
+    rescue
+      nil
+    end
+    
+    def store(name, filename, sign, obj)
+      File.open(filename, 'wb') do |f|
+        Marshal.dump(name, f)
+        Marshal.dump(sign, f)
+        Marshal.dump(obj, f)
+      end
+    rescue
+      unlink(filename)
+    end
+
+    def get(name, src)
+      return yield(name, src) if src.nil?
+
+      filename, sign = make_filename_and_sign(name, src)
+      filename = File.join(@dir, filename)
+      obj = fetch(name, filename, sign)
+
+      unless obj
+        obj = yield(name, src)
+        store(name, filename, sign, obj)
+      end
+      obj
+    end
+
+    def make_filename_and_sign(name, src)
+      md5 = Digest::MD5.new
+
+      md5.update(name)
+      filename = md5.hexdigest
+      
+      md5.update(src)
+      sign = md5.hexdigest
+
+      [filename, sign]
+    end
+  end
+
+  class NullContentCache
+    def get(name, src)
+      yield(name, src)
+    end
+  end
+end
+
+module RWiki
+  class Page
+    @@content_cache = NullContentCache.new
+    
+    def self.install_content_cache(c)
+      @@content_cache = c
+    end
+
+    def make_content(v)
+      @@content_cache.get(@name, v) do |name, v|
+        Content.new(name, v)
+      end
+    end
+  end
+end
