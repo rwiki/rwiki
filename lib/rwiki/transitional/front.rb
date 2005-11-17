a6bd7a597e89d44fa7f5f53524ac2c7e541f4c6e
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2005-11-17 08:35:41 +0000

    convert the fragment of URL using JavaScript
    git-svn-id: http://www.cozmixng.org/repos/rwiki/trunk@301 6ac77252-39e2-0310-ae13-de3c1d0591de

--- lib/rwiki/transitional/front.rb
+++ lib/rwiki/transitional/front.rb
@@ -1,12 +1,14 @@
 # -*- indent-tabs-mode: nil -*-
 
+require "erb"
+require "iconv"
 require "rwiki/front"
 require "rwiki/transitional/format"
-require "iconv"
 
 module RWiki
   module Transitional
     class Front < ::RWiki::Front
+      include ERB::Util
 
       def initialize(book, new_enc, old_enc)
         @book = book
@@ -16,11 +18,15 @@ module RWiki
 
       private
 
+      def convert(name)
+        Iconv.conv(@new_enc, @old_enc, name)
+      end
+
       def do_get_view(req, env={}, &block)
         pagename = req.name
         page = @book[pagename]
         if page.empty?
-          converted_name = Iconv.conv(@new_enc, @old_enc, pagename)
+          converted_name = convert(pagename)
           page = @book[converted_name]
           unless page.empty?
             return RedirectFormat.new(env, &block).redirect(page)
@@ -29,6 +35,22 @@ module RWiki
         super(req, env, &block)
       end
 
+      def do_get_convert(req, env={}, &block)
+        name = req.name
+        name.sub!(/\A\#/, '')
+        name.gsub!(/((?:\.[0-9a-fA-F]{2})+)/n) do
+          [$1.delete('.')].pack('H*')
+        end
+        name = convert(name)
+        # see label2anchor() in lib/rwiki/rd/rd2rwiki-lib.rb
+        fragment = name.gsub(/([^A-Za-z0-9\-_]+)/n) {
+          '.' + $1.unpack('H2' * $1.size).join('.')
+        }
+        return h(fragment) + "\n" + h(name)
+      end
+
     end
   end
+
+  Request::COMMAND << "convert"
 end
