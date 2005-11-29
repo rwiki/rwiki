0beaf8de312e7fb804e75fe94235fef6e939bc36
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2005-11-29 07:36:42 +0000

    use RWiki::Encode::PunycodeMark
    git-svn-id: http://www.cozmixng.org/repos/rwiki/trunk@316 6ac77252-39e2-0310-ae13-de3c1d0591de

--- site/ruby-lang/site/ruby-man-config-utf8.rb
+++ site/ruby-lang/site/ruby-man-config-utf8.rb
@@ -16,8 +16,8 @@ module RWiki
   #TITLE = "Ruby\245\352\245\325\245\241\245\354\245\363\245\271\245\336\245\313\245\345\245\242\245\353" # euc-jp
 
   require 'rwiki/encode'
-  ::RWiki::Encode.use_punycode
-  TOP_NAME = 'p-Ruby-jk4cg4ntfpd9dsdscui9h' # utf-8 and p_encode
+  Encode.use_punycode
+  TOP_NAME = Encode::PunycodeMark + 'Ruby-jk4cg4ntfpd9dsdscui9h' # utf-8 and p_encode
   #TOP_NAME = "Ruby\343\203\252\343\203\225\343\202\241\343\203\254\343\203\263\343\202\271\343\203\236\343\203\213\343\203\245\343\202\242\343\203\253" # utf-8
   TITLE = "Ruby\343\203\252\343\203\225\343\202\241\343\203\254\343\203\263\343\202\271\343\203\236\343\203\213\343\203\245\343\202\242\343\203\253" # utf-8
   DRB_URI = "druby://localhost:7429"
