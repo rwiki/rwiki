f0a6af131a32dda937d64edcafc3c2898207a472
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2006-08-06 04:21:26 +0000

    hilight bug fix noscript bug fix
    git-svn-id: http://www.cozmixng.org/repos/rwiki/trunk@420 6ac77252-39e2-0310-ae13-de3c1d0591de

--- lib/rwiki/rd/ext/redirect.rb
+++ lib/rwiki/rd/ext/redirect.rb
@@ -8,10 +8,15 @@ module RD
       extend RWiki::GetText
 
       def ext_block_verb_redirect(label, content, visitor)
-          return nil unless /^REDIRECT (.+)$/ =~ label
-          pagename = $1
-          url = "<%=h ref_name(#{pagename.dump})%>"
-          %Q!<script type="text/javascript">location.href = "#{url}"</script><noscript><a href="#{url}">Click here and go to `#{url}'</a>!
+        return nil unless /^REDIRECT (.+)$/ =~ label
+        pagename = $1
+        url = "<%=h ref_name(#{pagename.dump})%>"
+        <<-"RHTML"
+<p><a href="#{url}">Click here and go to `#{url}'</a><p>
+<script type="text/javascript"><!--
+location.href = "#{url}";
+//--></script>
+        RHTML
       end
       def self.about_ext_block_verb_redirect
         h(_("If first line is `# REDIRECT PageName', redirect to PageName."))
