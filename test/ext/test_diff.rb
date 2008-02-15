b60cd7c2dbb347f624631ad29c44afe3f49cc1f2
Kouhei Sutou <kou@cozmixng.org>
2008-02-15 05:58:08 +0000

    * implemented find_longest_match.
    git-svn-id: http://www.cozmixng.org/repos/rwiki/trunk@450 6ac77252-39e2-0310-ae13-de3c1d0591de

--- test/ext/test_diff.rb
+++ test/ext/test_diff.rb
@@ -4,6 +4,18 @@ class TestExtDiff < Test::Unit::TestCase
                       ["abc def", "abc", "abc def"])
   end
 
+  def test_longest_match
+    assert_longest_match([0, 1, 3],
+                         %w(b c d), %w(a b c d x y z),
+                         0, 2, 0, 7)
+    assert_longest_match([1, 2, 2],
+                         %w(b c d), %w(a b c d x y z),
+                         1, 2, 0, 6)
+    assert_longest_match([0, 0, 0],
+                         %w(a b), %w(c),
+                         0, 1, 0, 0)
+  end
+
   def test_same_contents
     # assert_ndiff(["  aaa"], ["aaa"], ["aaa"])
   end
@@ -12,6 +24,14 @@ class TestExtDiff < Test::Unit::TestCase
   def assert_to_indexes(expected, to)
     matcher = Test::Diff::SequenceMatcher.new([""], to)
     assert_equal(expected, matcher.instance_variable_get("@to_indexes"))
+  end
+
+  def assert_longest_match(expected, from, to,
+                           from_start, from_end,
+                           to_start, to_end)
+    matcher = Test::Diff::SequenceMatcher.new(from, to)
+    assert_equal(expected, matcher.longest_match(from_start, from_end,
+                                                 to_start, to_end))
   end
 
   def assert_ndiff(expected, from, to)
--- test/test-unit-ext/diff.rb
+++ test/test-unit-ext/diff.rb
@@ -9,6 +9,38 @@ module Test
         update_to_indexes
       end
 
+      def longest_match(from_start, from_end, to_start, to_end)
+        best_from, best_to, best_size = from_start, to_start, 0
+        lengths = {}
+        from_start.upto(from_end) do |i|
+          new_lengths = {}
+          (@to_indexes[@from[i]] || []).each do |j|
+            next if j < to_start
+            break if j >= to_end
+            k = new_lengths[j] = (lengths[j - 1] || 0) + 1
+            if k > best_size
+              best_from, best_to, best_size = i - k + 1, j - k + 1, k
+            end
+          end
+          lengths = new_lengths
+        end
+
+        while best_from > from_start and best_to > to_start and
+            @from[best_from - 1] == @to[best_to - 1]
+          best_from -= 1
+          best_to -= 1
+          best_size += 1
+        end
+
+        while best_from + best_size <= from_end and
+            best_to + best_size <= to_end and
+            @from[best_from + best_size] == @to[best_to + best_size]
+          best_size += 1
+        end
+
+        [best_from, best_to, best_size]
+      end
+
       private
       def update_to_indexes
         @to_indexes = {}
