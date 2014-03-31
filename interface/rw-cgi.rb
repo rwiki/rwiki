4d91f2ff38f0e01da3b885ffde910557e28c4478
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2014-03-31 19:54:29 +0900

    move Logger to front

--- interface/rw-cgi.rb
+++ interface/rw-cgi.rb
@@ -2,16 +2,15 @@
 
 require 'drb/drb'
 
-require 'rwiki/cgi'
-require 'rwiki/service'
-
 rwiki_uri = 'druby://localhost:8470'		# SETUP
-rwiki_log_file = '/var/tmp/rw-cgi.log'		# SETUP
 
-DRb.start_service()
-log_level = Logger::Severity::INFO
+DRb.start_service('druby://localhost:0')
 rwiki = DRbObject.new_with_uri(rwiki_uri)
-service = RWiki::Service.new(rwiki, log_level)
-service.set_log(rwiki_log_file, 'weekly')
-cgi = RWiki::CGI.new({}, service)
-cgi.start
+begin
+  rwiki.cgi_start(ENV.to_hash, $stdin, $stdout)
+rescue
+  puts "content-type: text/plain;"
+  puts
+  puts "RWiki server seems to be down..."
+end
+
