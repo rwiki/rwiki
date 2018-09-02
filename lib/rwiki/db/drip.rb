3b6680be0b4f12858dd9087d1c89e606422f7076
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2018-09-02 16:31:59 +0900

    add DripDB

---
+++ lib/rwiki/db/drip.rb
@@ -0,0 +1,63 @@
+require 'rwiki/db/base'
+
+module RWiki
+  module DB
+    class Drip < Base
+      def initialize(drip)
+        @drip = drip
+        super()
+      end
+
+      private
+      def drip_get(key, rev=nil)
+        if rev
+          mod, value, tag, = @drip.read(rev.to_i(36), 1, 0).first
+          unless key == tag
+            return nil, nil
+          end
+        else
+          mod, value, = @drip.head(1, key).first
+        end
+        return nil, nil if value.empty?
+        return mod, value
+      end
+
+      def drip_set(key, value)
+        @drip.write(value, key)
+      end
+
+      def set(key, value, opt=nil)
+        return if value.nil?
+        drip_set(key, value)
+        nil
+      end
+
+      def get(key, rev=nil)
+        mod, value = drip_get(key, rev)
+        value
+      rescue
+        nil
+      end
+
+      public
+      def modified(key)
+        mod, value = drip_get(key)
+        @drip.key_to_time(mod)
+      rescue
+        nil
+      end
+
+      def revision(key)
+        mod, = drip_get(key)
+        mod.to_s(36)
+      end
+
+      def each
+        cur = ''
+        while cur = @drip.tag_next(cur)
+          yield(cur)
+        end
+      end
+    end
+  end
+end
